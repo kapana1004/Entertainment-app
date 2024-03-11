0b36ddcab21197c63864c1b6abd8cfbc0931f3fe
George Kapanadze <52297225+kapana1004@users.noreply.github.com>
2024-03-11 18:39:50 +0400

    add handleToggleLoginPasswordVisibility function for hide or show the password

--- src/components/Logincomponent.tsx
+++ src/components/Logincomponent.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./SVG/Logo";
 
 export default function Logincomponent() {
+  const [showLoginPassword, setShowLoginPassword] = useState<boolean>(false);
+
+  const handleToggleLoginPasswordVisibility = (): void => {
+    setShowLoginPassword(!showLoginPassword);
+  };
   return (
     <div className=" min-w-[100vw] min-h-[100vh] bg-[#10141E] flex flex-col items-center">
       {/* <img className=" mt-[40px]" src={Logo} alt="logo" /> */}
@@ -19,11 +24,21 @@ export default function Logincomponent() {
           placeholder="Email address"
           type="text"
         />
-        <input
-          className=" w-[279px] h-[37px] ml-[20px] text-[#FFFFFF] bg-[#161D2F] pl-[24px] pb-[35px] pt-[35px] border-b border-b-[#5A698F]"
-          placeholder="Password"
-          type="text"
-        />
+
+        <div className=" relative">
+          <input
+            className=" w-[279px] h-[37px] ml-[20px] text-[#FFFFFF] bg-[#161D2F] pl-[24px] pb-[35px] pt-[35px] border-b border-b-[#5A698F]"
+            placeholder="Password"
+            type={showLoginPassword ? "text" : "password"}
+          />
+          <span
+            onClick={handleToggleLoginPasswordVisibility}
+            className=" cursor-pointer text-[10px] text-red-800 h-[15px] absolute top-[40%] right-[15%] w-[15px]"
+          >
+            {" "}
+            {showLoginPassword ? "hide" : "show"}
+          </span>
+        </div>
 
         <button className=" ml-[20px] mt-[30px] w-[279px] h-[48px] rounded-[10px] bg-[#FC4747] text-[15px] text-[#FFFFFF]">
           {" "}
