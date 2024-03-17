02b747022e836540325cdbbf04a12a5066e06c72
George Kapanadze <52297225+kapana1004@users.noreply.github.com>
2024-03-17 21:31:36 +0400

    import data.json and use it in mainpage

--- src/pages/Mainpage.tsx
+++ src/pages/Mainpage.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Menubar from "./Menubar";
 import IconSearch from "../../public/assets/icon-search.svg";
+import ContentData from "../data.json";
+
+console.log(ContentData);
 export default function Mainpage() {
   return (
-    <div className="flex min-w-[100vw] min-h-[100vh] flex-col bg-[#10141E]">
+    <div className="flex min-w-[100vw] min-h-[100vh] flex-col bg-[#10141E] items-center ">
       <Menubar />
-      <div className=" flex relative pt-[20px] w-[100%]">
+      <div className=" flex relative pt-[20px] w-[375px] ">
         <input
-          className=" bg-inherit text-[16px] w-[257px] h-[24px] text-[#FFFFFF] pl-[40px]"
+          className=" bg-inherit text-[16px] w-[257px] h-[24px] text-[#FFFFFF] pl-[40px] max-w-[257px] "
           type="search"
           placeholder="movies"
         />
@@ -16,6 +19,14 @@ export default function Mainpage() {
           src={IconSearch}
           alt="search"
         />
+      </div>
+      <div className="">
+        {ContentData.map((item, index) => (
+          <div className=" overflow-x-auto w-[240px]" key={index}>
+            <img className="" src={item.thumbnail.trending?.small} alt="" />
+          </div>
+        ))}
+        {}
       </div>
     </div>
   );
